8486c711a9a4106979c11f53fd84887b70bca6eb
Aziz Mansour <azizmansour@Azizs-MacBook-Air.local>
2025-11-24 07:41:52 -0800

    c finis

--- reservation.php
+++ reservation.php
@@ -1,2 +1,20 @@
 <?php
 require "db.php";
+
+$id = $_GET["id"];
+
+$stmt = $pdo->prepare("SELECT * FROM booking WHERE id = ?");
+$stmt->execute([$id]);
+$salle = $stmt->fetch();
+
+if (!$salle) {
+    die("Salle inexistante.");
+}
+
+$newState = $salle["is_available"] ? 0 : 1;
+
+$stmt = $pdo->prepare("UPDATE booking SET is_available = ? WHERE id = ?");
+$stmt->execute([$newState, $id]);
+
+header("Location: salles.php");
+exit;
--- salles.php
+++ salles.php
@@ -1,2 +1,30 @@
 <?php
 require "db.php";
+$stmt = $pdo->query("SELECT * FROM booking");
+$salles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<h1>Liste des salles</h1>
+
+<a href="ajout-salle.php">Ajouter une salle</a>
+<br><br>
+
+<table border="1" width="50%">
+    <tr>
+        <th>ID</th>
+        <th>Salle</th>
+        <th>Disponible ?</th>
+        <th>Actions</th>
+    </tr>
+
+    <?php foreach ($salles as $salle): ?>
+        <tr style="background-color: <?= $salle['is_available'] ? 'white' : 'red' ?>;">
+            <td><?= $salle['id'] ?></td>
+            <td><?= $salle['room_number'] ?></td>
+            <td><?= $salle['is_available'] ? 'Oui' : 'Non' ?></td>
+            <td>
+                <a href="reservation.php?id=<?= $salle['id'] ?>">Changer état</a> |
+                <a href="supprimer-salle.php?id=<?= $salle['id'] ?>">Supprimer</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
